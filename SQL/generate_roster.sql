1417e5fa4d575167bc1eaadb84b1326fa74d7a93
Jin <jinwoo.hwang99@gmail.com>
2021-01-10 20:55:08 -0500

    Fixed SQL file

--- SQL/generate_roster.sql
+++ SQL/generate_roster.sql
@@ -1,62 +1,69 @@
--- CREATE DATABASE IF NOT EXISTS players;
-
-
--- USE players;
-
--- CREATE TABLE IF NOT EXISTS players_table ( ID smallint unsigned not null AUTO_INCREMENT, name varchar(20) not null, position varchar(20) not null, \
--- school varchar(50) not null, primary key (ID) );
-
-
--- DECLARE name_str  VARCHAR(255);
--- DECLARE position_str  VARCHAR(255);
--- DECLARE school_str  VARCHAR(255);
-
-
--- SET name_str =  'Hinata';
--- set position_str = 'M';
--- SET school_str = 'Opposite';
-
--- INSERT INTO players_table ( name, gender, disease ) VALUES ( name_str, position_str, school_str );
-
-# SQL Script that creates fake player data
-# Create the database parameters; ID, Name, Position, School
 CREATE DATABASE IF NOT EXISTS players;
+
+
 USE players;
 
 CREATE TABLE IF NOT EXISTS players_table ( ID smallint unsigned not null AUTO_INCREMENT, name varchar(20) not null, position varchar(20) not null, \
 school varchar(50) not null, primary key (ID) );
 
 
-# Create a loop function; Generates 100 players
-DELIMITER $$
+DECLARE name_str  INT;
+DECLARE position_str  INT;
+DECLARE school_str  INT;
+DECLARE serve_int  INT;
+DECLARE serve_reception_int  INT;
+DECLARE dig_int  INT;
+DECLARE set_int  INT;
+DECLARE spike_int  INT;
+DECLARE block_int  INT;
+DECLARE total  INT;
+DECLARE notes  INT;
 
-USE players
+SET name_str =  'Kageyama';
+set position_str = 'M';
+SET school_str = 'Opposite';
 
-CREATE PROCEDURE test_mysql_while_loop()
-BEGIN
-DECLARE id_int  INT;
-DECLARE name_str  VARCHAR(255);
-DECLARE position_str  VARCHAR(255);
-DECLARE school_str  VARCHAR(255);
+INSERT INTO players_table ( name, gender, disease ) VALUES ( name_str, position_str, school_str );
 
-SET id_int = 1;
+-- # SQL Script that creates fake player data
+-- # Create the database parameters; ID, Name, Position, School
+-- CREATE DATABASE IF NOT EXISTS players;
+-- USE players;
 
-WHILE id_int  <= 100 DO
-
-SET name_str =  (select elt(0.5 + RAND() * 5, 'Hinata', 'Kageyama', 'Atsumu', 'Hoshiumi', 'Sakusa', 'Oikawa'));
-set position_str = (select elt(0.5 + RAND() * 2, 'Opposite', 'Setter'));
-SET school_str = (select elt(0.5 + RAND() * 6, 'nize', 'ur', 'bique', 'fam', 'why you', 'souka'));
+-- CREATE TABLE IF NOT EXISTS players_table ( ID smallint unsigned not null AUTO_INCREMENT, name varchar(20) not null, position varchar(20) not null, \
+-- school varchar(50) not null, primary key (ID) );
 
 
-SET  id_int = id_int + 1; 
-END WHILE;
+-- # Create a loop function; Generates 100 players
+-- DELIMITER $$
 
-SELECT * FROM ;
-END$$
+-- USE players
 
-DELIMITER ;
+-- CREATE PROCEDURE test_mysql_while_loop()
+-- BEGIN
+-- DECLARE id_int  INT;
+-- DECLARE name_str  VARCHAR(255);
+-- DECLARE position_str  VARCHAR(255);
+-- DECLARE school_str  VARCHAR(255);
 
-# Call the loop/generate players function
-USE players;
+-- SET id_int = 1;
 
-CALL test_mysql_while_loop();
+-- WHILE id_int  <= 100 DO
+
+-- SET name_str =  (select elt(0.5 + RAND() * 5, 'Hinata', 'Kageyama', 'Atsumu', 'Hoshiumi', 'Sakusa', 'Oikawa'));
+-- set position_str = (select elt(0.5 + RAND() * 2, 'Opposite', 'Setter'));
+-- SET school_str = (select elt(0.5 + RAND() * 6, 'nize', 'ur', 'bique', 'fam', 'why you', 'souka'));
+
+
+-- SET  id_int = id_int + 1; 
+-- END WHILE;
+
+-- SELECT * FROM ;
+-- END$$
+
+-- DELIMITER ;
+
+-- # Call the loop/generate players function
+-- USE players;
+
+-- CALL test_mysql_while_loop();
